cba4e6f3dc9f37338d602755f215571eabe54c13
Kang L <143698732+Kangeldzi@users.noreply.github.com>
2026-09-23 08:15:12 +0700

    Add Firebase configuration file

---
+++ public/firebase-config.js
@@ -0,0 +1,17 @@
+// firebase-config.js
+// Konfigurasi Firebase + URL backend SALINK
+
+const SALINK_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD1Z4LTTsLaeKWx-RF2wbZR7GXjqiAP3tE",
+  authDomain: "salink-app.firebaseapp.com",
+  projectId: "salink-app",
+  storageBucket: "salink-app.firebasestorage.app",
+  messagingSenderId: "619188691915",
+  appId: "1:619188691915:web:291c1cc49f8a30ff090c4e"
+};
+
+// VAPID Key dari Firebase Console → Cloud Messaging → Web Push certificates
+const SALINK_VAPID_KEY = "BGwJ9E-bZh2QwdYIeaSyVoxSc9puUnu6mjsMeR2nr9FAUSKAaq0SIuIlBxdaUExuYTd63zriEZwOTjj6TPTlMa0";
+
+// URL Web App Apps Script (yang ada /exec di akhir)
+const SALINK_BACKEND_URL = "https://script.google.com/macros/s/AKfycbzSxSHnPpyNi-W0p_5tXV4fCXHLpMXX3nrLVCLmiSRrpxqDNSN2CZcLlpZqThpryjPj/exec";
